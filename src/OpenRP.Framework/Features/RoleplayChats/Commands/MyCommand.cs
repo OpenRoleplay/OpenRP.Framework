f5030d620efa224526a057ac8cbed1cd83d79345
Benj98 <38333326+Benj98@users.noreply.github.com>
2025-02-16 19:05:28 +0100

    Create MyCommand.cs

---
+++ src/OpenRP.Framework/Features/RoleplayChats/Commands/MyCommand.cs
@@ -0,0 +1,29 @@
+﻿using OpenRP.Framework.Features.Commands.Attributes;
+using OpenRP.Framework.Shared.Chat.Enums;
+using SampSharp.Entities.SAMP;
+using SampSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRP.Framework.Features.Players.Extensions;
+using OpenRP.Framework.Shared.Chat.Services;
+
+namespace OpenRP.Framework.Features.RoleplayChats.Commands
+{
+    class MyCommand : ISystem
+    {
+        [ServerCommand(PermissionGroups = new string[] { "Default" },
+            Description = "Describe something related to your character. For example, /my face is cut would display \"Player's face is cut\" in chat to nearby players.",
+            CommandGroups = new[] { "Chat" })]
+
+        public void My(Player player, IChatService chatService, string text)
+        {
+            if (player.IsPlayerPlayingAsCharacter())
+            {
+                chatService.SendPlayerChatMessage(player, PlayerChatMessageType.MY, text);
+            }
+        }
+    }
+}
