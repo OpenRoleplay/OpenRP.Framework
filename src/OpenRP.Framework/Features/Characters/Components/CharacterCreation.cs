9dd7dc361e6cdbb07fc34bb34ac7823c72c19e1a
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-02-16 13:00:18 +0100

    CharacterCreation component

---
+++ src/OpenRP.Framework/Features/Characters/Components/CharacterCreation.cs
@@ -0,0 +1,20 @@
+﻿using OpenRP.Framework.Database.Models;
+using SampSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.Characters.Components
+{
+    public class CharacterCreation : Component
+    {
+        public CharacterModel CreatingCharacter { get; set; }
+
+        public CharacterCreation()
+        {
+            CreatingCharacter = new CharacterModel();
+        }
+    }
+}
