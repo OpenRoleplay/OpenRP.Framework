6b855e9866ca6456843740f439d7f6b4e03b07a0
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-02-19 19:15:13 +0100

    Fix

--- src/OpenRP.Framework/Features/Discord/Systems/DiscordSystem.cs
+++ src/OpenRP.Framework/Features/Discord/Systems/DiscordSystem.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenRP.Framework.Features.Players.Extensions;
 
 namespace OpenRP.Framework.Features.Discord.Systems
 {
