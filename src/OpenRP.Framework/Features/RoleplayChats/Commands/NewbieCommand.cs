10698eeb9aad1e8743d515d7645980b624cb8d8e
Benj98 <38333326+Benj98@users.noreply.github.com>
2025-02-20 20:00:05 +0100

    Newbie cooldown

--- src/OpenRP.Framework/Features/RoleplayChats/Commands/NewbieCommand.cs
+++ src/OpenRP.Framework/Features/RoleplayChats/Commands/NewbieCommand.cs
@@ -1,10 +1,12 @@
 ﻿using OpenRP.Framework.Features.Commands.Attributes;
 using OpenRP.Framework.Features.Players.Extensions;
 using OpenRP.Framework.Shared.Chat.Enums;
+using OpenRP.Framework.Shared.Chat.Extensions;
 using OpenRP.Framework.Shared.Chat.Services;
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,8 @@ namespace OpenRP.Framework.Features.RoleplayChats.Commands
 {
     public class NewbieCommand : ISystem
     {
+        private readonly ConcurrentDictionary<int, DateTime> _lastNewbieMessage = new ConcurrentDictionary<int, DateTime>();
+
         [ServerCommand(PermissionGroups = new string[] { "Default" },
             Description = "Send a message to the newbie chat. Use /newbie followed by your message to ask other players server-related questions.",
             CommandGroups = new[] { "Chat" })]
@@ -21,6 +25,19 @@ namespace OpenRP.Framework.Features.RoleplayChats.Commands
         { 
             if (player.IsPlayerPlayingAsCharacter())
             {
+                var now = DateTime.UtcNow;
+
+                if (_lastNewbieMessage.TryGetValue(player.Entity.Handle, out DateTime lastUsed))
+                {
+                    var elapsed = now - lastUsed;
+                    if (elapsed.TotalSeconds < 30)
+                    {
+                        player.SendPlayerInfoMessage(PlayerInfoMessageType.ERROR, $"You must wait {30 - elapsed.TotalSeconds:0} seconds before sending another newbie chat message.");
+                        return;
+                    }
+                }
+                _lastNewbieMessage.AddOrUpdate(player.Entity.Handle, now, (id, old) => now);
+
                 chatService.SendPlayerChatMessage(player, PlayerChatMessageType.NEWBIE, text);
             }
         }
@@ -30,6 +47,12 @@ namespace OpenRP.Framework.Features.RoleplayChats.Commands
         public void N(Player player, IChatService chatService, string text)
         {
             Newbie(player, chatService, text);
+        }
+
+        [Event]
+        public void OnPlayerDisconnect(Player player)
+        {
+            _lastNewbieMessage.TryRemove(player.Entity.Handle, out _);
         }
     }
 }
