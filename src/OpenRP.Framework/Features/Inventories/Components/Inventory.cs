18d0b06fee1ef27b2b1ded2e87de2958621b1991
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-02-23 14:49:23 +0100

    Refactor Inventory class to include additional methods for weight calculations and inventory dialog name generation

--- src/OpenRP.Framework/Features/Inventories/Components/Inventory.cs
+++ src/OpenRP.Framework/Features/Inventories/Components/Inventory.cs
@@ -1,4 +1,5 @@
 ﻿using OpenRP.Framework.Database.Models;
+using OpenRP.Framework.Database.Services;
 using OpenRP.Framework.Features.Items.Components;
 using SampSharp.Entities;
 using System;
@@ -33,9 +34,18 @@ namespace OpenRP.Framework.Features.Inventories.Components
             return _inventoryModel.Id;
         }
 
-        public uint? GetMaxWeight()
+        public string GetName()
         {
-            return _inventoryModel.MaxWeight;
+            return _inventoryModel.Name;
+        }
+
+        public uint GetMaxWeight()
+        {
+            if (_inventoryModel.MaxWeight != null && _inventoryModel.MaxWeight.HasValue)
+            {
+                return _inventoryModel.MaxWeight.Value;
+            }
+            return uint.MaxValue;
         }
 
         public List<InventoryItem> GetInventoryItems()
@@ -55,6 +65,42 @@ namespace OpenRP.Framework.Features.Inventories.Components
                     return itemInstance.IsItemInventory() &&
                         item.GetAdditionalData().GetString("INVENTORY") == inv.GetId().ToString();
                 }));
+        }
+
+        public uint GetAvailableWeight()
+        {
+            uint maxWeight = GetMaxWeight();
+
+            if(GetMaxWeight() == 0)
+            {
+                return 0;
+            }
+
+            List<InventoryItem> inventoryItems = GetInventoryItems();
+            if (inventoryItems == null || inventoryItems.Count == 0)
+            {
+                return maxWeight;
+            }
+
+            uint totalWeight = inventoryItems.Aggregate(0u, (sum, item) => sum + item.GetTotalWeight());
+
+            uint availableWeight = maxWeight - totalWeight;
+
+            return availableWeight > 0 ? availableWeight : 0;
+        }
+
+        public string GetInventoryDialogName(bool showWeight = true)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}", GetName());
+
+            if (showWeight)
+            {
+                sb.AppendFormat(" ({0}g / {1}g)", GetInventoryItems().Sum(i => i.GetTotalWeight()), GetMaxWeight());
+            }
+
+            return sb.ToString();
         }
 
         public bool DoesItemFit(Item item, uint amount)
