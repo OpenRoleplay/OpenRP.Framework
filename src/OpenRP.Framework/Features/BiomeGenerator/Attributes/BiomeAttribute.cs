909d2699d2355c743fac70d213f777f69d42b662
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-03-02 15:11:37 +0100

    Biome generator attribute & types

---
+++ src/OpenRP.Framework/Features/BiomeGenerator/Attributes/BiomeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using OpenRP.Framework.Features.BiomeGenerator.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.BiomeGenerator.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public class BiomeAttribute : Attribute
+    {
+        public string Name { get; }
+        public byte SpawnFrequency { get; }
+        public BiomeType BiomeType { get; }
+
+        public BiomeAttribute(byte spawnFrequency, string name, BiomeType biomeType)
+        {
+            Name = name;
+            SpawnFrequency = spawnFrequency;
+            BiomeType = biomeType;
+        }
+    }
+}
---
+++ src/OpenRP.Framework/Features/BiomeGenerator/Enums/BiomeType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.BiomeGenerator.Enums
+{
+    public enum BiomeType
+    {
+        BirchForest,
+        ConiferousWoodland,
+        DeadForest,
+        DeadForestWithTrees,
+        Farmland,
+        ForestPlains,
+        OakForest,
+        Plains,
+        SunflowerFields
+    }
+}
