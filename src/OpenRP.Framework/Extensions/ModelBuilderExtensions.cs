485c24d6f3c51aee1ab1f7bf314d02ba435fb83b
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-03-08 12:00:52 +0100

    InventoryDefaultData

--- src/OpenRP.Framework/Extensions/ModelBuilderExtensions.cs
+++ src/OpenRP.Framework/Extensions/ModelBuilderExtensions.cs
@@ -8,6 +8,7 @@ using OpenRP.Framework.Features.Currencies.Models.DefaultData;
 using OpenRP.Framework.Features.DroppedItems.Models.Configurations;
 using OpenRP.Framework.Features.Fishing.Models;
 using OpenRP.Framework.Features.Inventories.Models.Configurations;
+using OpenRP.Framework.Features.Inventories.Models.DefaultData;
 using OpenRP.Framework.Features.Items.Models.Configurations;
 using OpenRP.Framework.Features.Items.Models.DefaultData;
 using OpenRP.Framework.Features.Permissions.Models.Configurations;
@@ -40,6 +41,8 @@ namespace OpenRP.Framework.Extensions
             modelBuilder.ApplyConfiguration(new FishLootModelConfiguration());
 
             modelBuilder.ApplyConfiguration(new InventoryItemModelConfiguration());
+
+            modelBuilder.ApplyConfiguration(new InventoryModelDefaultData());
 
             modelBuilder.ApplyConfiguration(new ItemModelConfiguration());
             modelBuilder.ApplyConfiguration(new ItemModelDefaultData());
---
+++ src/OpenRP.Framework/Features/Inventories/Models/DefaultData/InventoryModelDefaultData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OpenRP.Framework.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.Inventories.Models.DefaultData
+{
+    public class InventoryModelDefaultData : IEntityTypeConfiguration<InventoryItemModel>
+    {
+        public void Configure(EntityTypeBuilder<InventoryItemModel> builder)
+        {
+            builder.HasData(
+                new InventoryModel
+                {
+                    Id = 1
+                    , Name = "World Inventory"
+                    , MaxWeight = null,
+                }
+            );
+        }
+    }
+}
