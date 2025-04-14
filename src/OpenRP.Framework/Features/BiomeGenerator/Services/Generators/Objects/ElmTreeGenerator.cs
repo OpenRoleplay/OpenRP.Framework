e3ea0cb0d5a86abc51faa1f147cca0074c5ca0c7
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-04-15 00:05:27 +0200

    Redwood Tree Generator + Elm Tree Generator

---
+++ src/OpenRP.Framework/Features/BiomeGenerator/Services/Generators/Objects/ElmTreeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using OpenRP.Framework.Features.BiomeGenerator.Entities;
+using SampSharp.Entities.SAMP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.BiomeGenerator.Services.Generators.Objects
+{
+    public class ElmTreeGenerator : IBiomeObjectGenerator
+    {
+        public string ObjectType => "ElmTree";
+
+        public BiomeObject Generate(Vector2 virtualPosition, Vector3 gamePosition, Vector3 gameRotation, Vector3 defaultRotation, Vector3 maxAngleRotation, Color outputColor)
+        {
+            int[] obj_arr_trees = { 893, 768, 895, 894, 767, 891, 782, 887, 886, 772, 773, 774, 775, 778, 890, 780, 781 };
+
+            int modelId = obj_arr_trees[Random.Shared.Next(obj_arr_trees.Length)];
+
+            BiomeObject treeObject = new BiomeObject(
+                obj_arr_trees[Random.Shared.Next(obj_arr_trees.Length)],
+                virtualPosition,
+                gamePosition,
+                gameRotation,
+                outputColor
+            );
+
+            return treeObject;
+        }
+    }
+}
