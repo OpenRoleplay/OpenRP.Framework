cb4579cd461118f0a507eed93aeebf22cf3e069e
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-04-18 15:58:55 +0200

    Fogbelt Biome Test

---
+++ src/OpenRP.Framework/Features/BiomeGenerator/Services/Generators/Biomes/FogbeltForestBiome.cs
@@ -0,0 +1,53 @@
+﻿using OpenRP.Framework.Features.BiomeGenerator.Attributes;
+using OpenRP.Framework.Features.BiomeGenerator.Entities;
+using OpenRP.Framework.Features.BiomeGenerator.Enums;
+using OpenRP.Framework.Features.BiomeGenerator.Helpers;
+using SampSharp.Entities.SAMP;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.BiomeGenerator.Services.Generators.Biomes
+{
+    [Biome(255, "Fogbelt Forest", BiomeType.Fogbelt)]
+    public class FogbeltForestBiome : IBiome
+    {
+        private readonly IBiomeObjectFactory _factory;
+        private readonly WeightedRandom<string> _weightedRandom;
+        private readonly Color _biomeOutputColor;
+
+        public FogbeltForestBiome(IBiomeObjectFactory factory)
+        {
+            _factory = factory;
+            _weightedRandom = new WeightedRandom<string>(new Dictionary<string, int>
+            {
+                { "Grass", 100 },
+                { "RegularBush", 10 },
+                { "FogbeltBush", 10 },
+                { "RedwoodTree", 20 },
+                { "Flower", 48 },
+                { "Nothing", 807 }
+            });
+            _biomeOutputColor = GetBiomeOutputColor();
+        }
+
+        public ConcurrentBag<BiomeObject> Generate(Vector2 virtualPosition, Vector3 gamePosition, Vector3 gameRotation, Vector3 defaultRotation, Vector3 maxAngleRotation)
+        {
+            string selectedType = _weightedRandom.GetRandomItem();
+            var assets = new ConcurrentBag<BiomeObject>();
+
+            if (selectedType != "Nothing")
+            {
+                BiomeObject element = _factory.Generate(selectedType, virtualPosition, gamePosition, gameRotation, defaultRotation, maxAngleRotation, _biomeOutputColor);
+                assets.Add(element);
+            }
+
+            return assets;
+        }
+
+        public Color GetBiomeOutputColor() => new Color(163, 166, 111);
+    }
+}
