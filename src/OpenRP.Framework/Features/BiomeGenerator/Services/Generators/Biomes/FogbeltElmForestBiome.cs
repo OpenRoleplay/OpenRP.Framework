6ea8fcd71e514b42340d7ddf347f16a537ed476c
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-04-19 21:19:33 +0200

    Changes

--- src/OpenRP.Framework/Features/BiomeGenerator/Services/Generators/Biomes/FogbeltElmForestBiome.cs
+++ src/OpenRP.Framework/Features/BiomeGenerator/Services/Generators/Biomes/FogbeltElmForestBiome.cs
@@ -12,14 +12,26 @@ using System.Threading.Tasks;
 
 namespace OpenRP.Framework.Features.BiomeGenerator.Services.Generators.Biomes
 {
-    [Biome(100, "Elm Forest", BiomeType.Fogbelt)]
-    public class FogbeltElmForestBiome : ElmForestBiome
+    [Biome(100, "Fogbelt Elm Forest", BiomeType.Fogbelt)]
+    public class FogbeltElmForestBiome : IBiome
     {
         private readonly IBiomeObjectFactory _factory;
         private readonly WeightedRandom<string> _weightedRandom;
         private readonly Color _biomeOutputColor;
 
-        public FogbeltElmForestBiome(IBiomeObjectFactory factory) : base(factory) { }
+        public FogbeltElmForestBiome(IBiomeObjectFactory factory)
+        {
+            _factory = factory;
+            _weightedRandom = new WeightedRandom<string>(new Dictionary<string, int>
+            {
+                { "Grass", 100 },
+                { "FogbeltBush", 10 },
+                { "ElmTree", 15 },
+                { "Flower", 48 },
+                { "Nothing", 817 }
+            });
+            _biomeOutputColor = GetBiomeOutputColor();
+        }
 
         public ConcurrentBag<BiomeObject> Generate(Vector2 virtualPosition, Vector3 gamePosition, Vector3 gameRotation, Vector3 defaultRotation, Vector3 maxAngleRotation)
         {
