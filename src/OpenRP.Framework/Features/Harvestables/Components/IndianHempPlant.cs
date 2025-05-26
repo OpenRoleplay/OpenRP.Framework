174a1fcb748e79a52aed15351d2cc2b555bf58cc
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-05-26 14:32:21 +0200

    Harvestables WIP

---
+++ src/OpenRP.Framework/Features/Harvestables/Components/IndianHempPlant.cs
@@ -0,0 +1,83 @@
+﻿using OpenRP.Framework.Features.BiomeGenerator.Entities;
+using OpenRP.Framework.Shared;
+using OpenRP.Streamer;
+using SampSharp.ColAndreas.Entities.Services;
+using SampSharp.Entities.SAMP;
+using SampSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRP.Framework.Features.Harvestables.Entities;
+
+namespace OpenRP.Framework.Features.Harvestables.Components
+{
+    public class IndianHempPlant : Component, IHarvestableEntity
+    {
+        private BiomeObject _generatedObject;
+        private int _remainingLeaves;
+
+        public IndianHempPlant(BiomeObject generatedObject)
+        {
+            _generatedObject = generatedObject;
+            _remainingLeaves = 10;
+
+            CreateDynamicObject();
+            //CreateDynamicTextLabel();
+        }
+
+        public string GetTextLabelString()
+        {
+            return $"{ChatColor.Highlight}Indian Hemp (Apocynum Cannabinum)\n{ChatColor.White}{_remainingLeaves} leaves remaining\n\nUse {ChatColor.Highlight}/harvest hemp";
+        }
+
+        public Vector3 GetTextLabelPosition()
+        {
+            Vector3 textLabelPosition = new Vector3(_generatedObject.GamePosition.XY, _generatedObject.GamePosition.Z + 0.25f);
+            return textLabelPosition;
+        }
+
+        public void CreateDynamicTextLabel()
+        {
+            Vector3 textLabelPosition = GetTextLabelPosition();
+        }
+
+        public DynamicTextLabel GetDynamicTextLabel()
+        {
+            return GetComponentInChildren<DynamicTextLabel>();
+        }
+
+        public void UpdateDynamicTextLabel()
+        {
+            DynamicTextLabel dynamicTextLabel = GetDynamicTextLabel();
+            dynamicTextLabel.Text = GetTextLabelString();
+        }
+
+        public void CreateDynamicObject()
+        {
+            BiomeManager.CreateGeneratorObject(_generatedObject, Entity, _colAndreasService, _streamerService);
+        }
+
+        public DynamicObject GetDynamicObject()
+        {
+            return GetComponentInChildren<DynamicObject>();
+        }
+
+        public bool IsPlayerNearby(Player player)
+        {
+            Vector3 textLabelPosition = GetTextLabelPosition();
+            if (player.IsInRangeOfPoint(3.0f, textLabelPosition))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Harvest()
+        {
+            _remainingLeaves -= 1;
+            UpdateDynamicTextLabel();
+        }
+    }
+}
---
+++ src/OpenRP.Framework/Features/Harvestables/Services/IndianHempHarvestable.cs
@@ -0,0 +1,54 @@
+﻿using OpenRP.Framework.Features.Harvestables.Entities;
+using OpenRP.Framework.Shared.Chat.Enums;
+using SampSharp.Entities.SAMP;
+using SampSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRP.Framework.Shared.Chat.Extensions;
+using OpenRP.Streamer;
+using SampSharp.ColAndreas.Entities.Services;
+using OpenRP.Framework.Features.Harvestables.Components;
+
+namespace OpenRP.Framework.Features.Harvestables.Services
+{
+    public class IndianHempHarvestable : IHarvestable
+    {
+        private readonly IEntityManager _entityManager;
+        private readonly IColAndreasService _colAndreasService;
+        private readonly IStreamerService _streamerService;
+        public IndianHempHarvestable(IEntityManager entityManager, IColAndreasService colAndreasService, IStreamerService streamerService)
+        {
+            _entityManager = entityManager;
+            _streamerService = streamerService;
+            _colAndreasService = colAndreasService;
+        }
+
+        public string ResourceName => "industrial hemp";
+        public int ResourceObjectModelId => 19473;
+
+        public void CreateHarvestable(Vector3 positions, Vector3 rotations)
+        {
+
+        }
+
+        public void BeginHarvest(Player player)
+        {
+            foreach (IndianHempPlant indianHempPlant in _entityManager.GetComponents<IndianHempPlant>())
+            {
+                if (indianHempPlant.IsPlayerNearby(player))
+                {
+                    PlayerHarvesting.StartHarvesting(player, this, indianHempPlant, TimeSpan.FromSeconds(10));
+                    return;
+                }
+            }
+            player.SendPlayerInfoMessage(PlayerInfoMessageType.ERROR, "You are not near any harvestable Indian Hemp plants!");
+        }
+
+        public void EndHarvest(Player player)
+        {
+        }
+    }
+}
