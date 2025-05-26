8279e4c587f3efd62320d1127c980e530d578b38
Koert Lichtendonk <C.Lichtendonk@live.nl>
2025-05-26 14:02:07 +0200

    Add IHarvestable interface for handling resource harvesting in OpenRP.Framework

---
+++ src/OpenRP.Framework/Features/Harvestables/Entities/IHarvestable.cs
@@ -0,0 +1,34 @@
+﻿using SampSharp.Entities.SAMP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Features.Harvestables.Entities
+{
+    public interface IHarvestable
+    {
+        /// <summary>
+        /// The name of the resource (e.g. "hemp"). Used for the /harvest command.
+        /// </summary>
+        string ResourceName { get; }
+
+        /// <summary>
+        /// The object model id of the resource.
+        /// </summary>
+        string ResourceObjectModelId { get; }
+
+        /// <summary>
+        /// Performs code at the begin of a player harvesting the resource
+        /// </summary>
+        /// <param name="player">The player harvesting.</param>
+        void BeginHarvest(Player player);
+
+        /// <summary>
+        /// Performs code at the end of a player harvesting the resource
+        /// </summary>
+        /// <param name="player">The player harvesting.</param>
+        void EndHarvest(Player player);
+    }
+}
